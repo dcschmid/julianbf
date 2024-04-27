ed753848fd49eadca9c4a815f415ba4311f9dc12
Daniel Schmid <dcschmid@murena.io>
2024-04-27 22:54:11 +0200

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), playformCompress()]
+});
