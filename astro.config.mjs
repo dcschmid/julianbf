7b38b4a5f4da81eb1f71290822a1ec9988140344
Daniel Schmid <dcschmid@murena.io>
2024-04-28 14:47:19 +0200

    feat: add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import playformCompress from "@playform/compress";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), playformCompress()]
+  site: "https://www.julianbf.de",
+  integrations: [mdx(), playformCompress(), sitemap()],
 });
